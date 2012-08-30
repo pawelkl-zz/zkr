697c7f8b7b8cdbf8c6082b96f55f28e594663906
Pawel Klosiewicz <pawelkl@gmail.com>
2012-08-30 12:12:07 +0200

    fitvids...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
-// // = require jquery.fitvids.js
+// ----- // = requirAe jquery.fitvids.js
 
 // $(document).ready(function(){
 //   $(".pull-right").fitVids();
